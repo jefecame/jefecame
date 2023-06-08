48d4737eed203077497fc020a64fbb7b2184380f
Jesús Fernando Carrasco Mendoza <jefecame@hotmail.com>
2023-06-08 05:56:30 +0000

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
+string a =  "hello world";
 
 int main() {
   cin.tie(nullptr);
   ios_base::sync_with_stdio(false);
   
-  cout << "Hola mundo" << endl;
-  
+  cout << "Hola mundo " << a << endl;
+    
   return 0;
 }
